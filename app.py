9ee0c41acae16e02086aaa6577d704212b70f2ae
Meng Lin <meng.lin@outlook.com>
2023-06-10 09:50:52 +0100

    Simplify logic

--- app.py
+++ app.py
@@ -1,66 +1,34 @@
 import dotenv
 import os
 import streamlit as st
-import pdfplumber
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+import tempfile
+from langchain import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import RetrievalQA
 
 dotenv.load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-def read_pdf(path):
-    with pdfplumber.open(path) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+file = st.file_uploader("Upload a PDF file", type=["pdf"])
+if file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as tf:
+        tf.write(file.getbuffer())
+        file_path = tf.name
+    doc = PyPDFLoader(file_path).load()
 
-uploaded_file = st.file_uploader("Choose a PDF file", type = "pdf")
-if uploaded_file is not None:
-    # load PDf
-    with st.spinner("Loading PDf..."):
-        text = read_pdf(uploaded_file)
-    st.success("Finished loading PDF.")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+    docs = text_splitter.split_documents(doc)
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    knowledge_base = FAISS.from_documents(docs, embeddings)
 
-    # chunking PDF
-    text_splitter = CharacterTextSplitter(
-        separator = "\n",
-        chunk_size = 2000,
-        chunk_overlap = 200,
-        length_function = len
-    )
-    chunks = text_splitter.split_text(text)
-    embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
-
-    # prompt
-    prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-    {context}
-
-    Question: {question}
-    Answer:"""
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
-    
-    # question
-    query = st.text_input("Your question:")
-    response = ""
-    if query:
-        with st.spinner("Running query..."):
-            docs = knowledge_base.similarity_search(query)
-            
-            # print(str(docs))
-            # print(str(query))
-
-            llm = OpenAI(openai_api_key = openai_api_key, temperature = 0.7)
-            chain = load_qa_chain(llm, chain_type = "stuff", prompt = PROMPT)
-            response = chain.run(input_documents = docs, question = query)
-
-        st.success("Completed query.")
+    question = st.text_input("Ask your question here:")
+    if question:
+        llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+        chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=knowledge_base.as_retriever())
+        response = chain.run(question)
+        
+        st.success("Completed question.")
         st.write("Answer: ", response)
